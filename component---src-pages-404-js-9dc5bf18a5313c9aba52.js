7ae39e087483c44579d90c7a0a99aae9d02c13c6
Keita-Shimizu <keita@couger.co.jp>
2020-11-04 19:53:33 +0900

    Updates

---
+++ component---src-pages-404-js-9dc5bf18a5313c9aba52.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),l=t("Wbzz"),u=t("Bl7J");n.default=function(){return o.a.createElement(u.a,null,o.a.createElement("h1",null,"Page not found"),o.a.createElement(l.Link,{to:"/"},"Return to home"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9dc5bf18a5313c9aba52.js.map
