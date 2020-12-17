ce0f8fe6aa09ace7015bb2c169b1d96338f789ab
hirasaki1985 <hirasaki1985@gmail.com>
2020-12-17 18:49:01 +0900

    #20 レイアウト調整

--- src/components/main.js
+++ src/components/main.js
@@ -72,58 +72,57 @@ const Main = ({ data }) => {
         {/* activity */}
         <Activity />
 
-        <Row className="bg-beige mb-5">
-          <Container>
-            <Row className="m-5">
-              {/* ワーキンググループ */}
-              <Col className="col-sm">
-                <h4 className="text-center mb-5">
-                  <span role="img" aria-label="friendship">👬</span>
-                  <span role="img" aria-label="friendship different order">👫</span>
-                  {intl.formatMessage({ id: "wg" })}
-                </h4>
-                <ul>
-                  <li>{intl.formatMessage({ id: "wgList1" })}</li>
-                  <li>{intl.formatMessage({ id: "wgList2" })}</li>
-                  <li>{intl.formatMessage({ id: "wgList3" })}</li>
-                </ul>
-              </Col>
+        <Row>
+          <Col className="col-sm py-md-5 pb-5">
+            <h4 className="text-center mb-5">
+              <span role="img" aria-label="friendship">
+                👬
+              </span>
+              <span role="img" aria-label="friendship different order">
+                👫
+              </span>
+              {intl.formatMessage({ id: "wg" })}
+            </h4>
+            <ul>
+              <li>{intl.formatMessage({ id: "wgList1" })}</li>
+              <li>{intl.formatMessage({ id: "wgList2" })}</li>
+              <li>{intl.formatMessage({ id: "wgList3" })}</li>
+            </ul>
+          </Col>
+          <Col className="col-sm py-md-5 pb-5">
+            <h4 className="text-center mb-5">
+              <span role="img" aria-label="male researcher">
+                👨‍🔬
+              </span>
+              <span role="img" aria-label="female researcher">
+                👩‍⚕
+              </span>
+              {intl.formatMessage({ id: "ws" })}
+            </h4>
+            <ul>
+              <li>{intl.formatMessage({ id: "wsList1" })}</li>
+              <li>{intl.formatMessage({ id: "wsList2" })}</li>
+            </ul>
+          </Col>
+        </Row>
+        <ContactUs title={intl.formatMessage({ id: "contact" })} />
+      </Container>
 
-              {/* ワークショップ */}
-              <Col className="col-sm">
-                <h4 className="text-center mb-5">
-                  <span role="img" aria-label="male researcher">👨‍🔬</span>
-                  <span role="img" aria-label="female researcher">👩‍⚕</span>
-                  {intl.formatMessage({ id: "ws" })}
-                </h4>
-                <ul>
-                  <li>{intl.formatMessage({ id: "wsList1" })}</li>
-                  <li>{intl.formatMessage({ id: "wsList2" })}</li>
-                </ul>
-              </Col>
-            </Row>
-
-            {/* 参加対象 */}
-            <Row className="justify-content-center m-4 p-5" style={{backgroundColor: '#e4ddbb'}}>
-              <Col className="col-12 col-sm-12">
-                <h4 className="text-center mb-5">
-                  {intl.formatMessage({ id: "target" })}
-                </h4>
-                <ul>
-                  <li>{intl.formatMessage({ id: "target1" })}</li>
-                  <li>{intl.formatMessage({ id: "target2" })}</li>
-                  <li>{intl.formatMessage({ id: "target3" })}</li>
-                </ul>
-              </Col>
-            </Row>
-
-            {/* contact */}
-            <Row className="justify-content-center">
-              <ContactUs title={intl.formatMessage({ id: "contact" })} />
-            </Row>
-          </Container>
+      <Container fluid className="bg-beige">
+        <Row className="justify-content-md-center py-5 bg-beige">
+          <Col className="col-12 col-sm-6">
+            <h4 className="text-center mb-5">
+              {intl.formatMessage({ id: "target" })}
+            </h4>
+            <ul>
+              <li>{intl.formatMessage({ id: "target1" })}</li>
+              <li>{intl.formatMessage({ id: "target2" })}</li>
+              <li>{intl.formatMessage({ id: "target3" })}</li>
+            </ul>
+          </Col>
         </Row>
       </Container>
+
       <Container>
         <Row className="py-5">
           <Col>
