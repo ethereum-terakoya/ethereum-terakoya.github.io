70ef833aff93aa61da23b69a5c52236f2f2dd0a9
NelsonSuperSonic <nelson@supersonicdesign.com>
2022-11-20 01:27:16 -0400

    change size on thumbnail seo

--- src/templates/post.js
+++ src/templates/post.js
@@ -60,7 +60,7 @@ export const query = graphql`
         group
         image {
           childImageSharp {
-            fixed(width: 200) {
+            fixed(width: 500) {
               src
             }
           }
